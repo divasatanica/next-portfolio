9f40c672dcad52ada0b5db35190fef7561235586
divasatanica <jayhebes@vip.qq.com>
2024-09-01 22:37:59 +0800

    feat: add analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import "github-markdown-css";
 import { Header } from "./_components/Header";
@@ -19,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
+        <Analytics />
         <Header />
         <main className="p-24 pt-[92px] pb-4">
           <div style={{ maxWidth: 1254, margin: '0 auto' }}>
